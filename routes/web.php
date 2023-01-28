4fa38642ab54d0caf02dafd20f8ffc41909e7994
Lurzar_ <68995865+lurzar@users.noreply.github.com>
2023-01-29 02:59:51 +0800

    Setup vue 3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', [PagesController::class, 'index']);
